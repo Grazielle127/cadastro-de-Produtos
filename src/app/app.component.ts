d3a5e69c4119873e92d485b499816bef0a181699
FelipeGtr01 <Felipe_Gutierre1@outlook.com>
2022-05-10 09:36:00 -0300

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PlayTech Gaming - by Amates';
+  title = 'HORTIFRUTI DA GRAZI';
 }
